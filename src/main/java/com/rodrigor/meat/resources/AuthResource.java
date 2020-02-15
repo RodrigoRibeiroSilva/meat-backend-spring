12cf7c0e583b5ed375d9d77ae803725d1ac264b7
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2020-02-15 11:31:36 -0300

    Adicionada modificações no AuthResource

--- src/main/java/com/rodrigor/meat/resources/AuthResource.java
+++ src/main/java/com/rodrigor/meat/resources/AuthResource.java
@@ -1,18 +1,15 @@
 package com.rodrigor.meat.resources;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigor.meat.security.JWTUtil;
 import com.rodrigor.meat.security.UserSS;
-import com.rodrigor.meat.services.AuthService;
 import com.rodrigor.meat.services.UserService;
 
 @RestController
